4952d93f3a57ee5bb916550af68b3932cbcdb396
RumeoLola <rumeomololaz@gmail.com>
2023-05-17 19:58:11 -0400

    shopping is in progress

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function openNav() {
+function openNav()  {
     document.getElementById("mySidenav").style.width = "100%";
   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function addToCart()  {
+
+  var orderfood = document.querySelector("#foodname").textContent;
+
+  var message = "The following food items have been added to your cart " + orderfood ;
+  
+  console.log("The food item " + orderfood + " has been added to your cart")
+
+  while (true)  {
+    document.getElementById("cartitems").innerHTML = message;
   }
+
+}

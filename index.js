078ce8ddcccadf8bb64dbe0acbf083446f002e61
RumeoLola <rumeomololaz@gmail.com>
2023-05-22 19:31:31 -0400

    will soon start working the ajax file

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 function openNav()  {
-    document.getElementById("mySidenav").style.width = "100%";
-  }
-  
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
 function addToCart()  {
-
   var orderfood = document.querySelector("#foodname").textContent;
-
+  
   var message = "The following food items have been added to your cart " + orderfood ;
   
   console.log("The food item " + orderfood + " has been added to your cart")
-
+  
   document.getElementById("cartitems").innerHTML = message
 
 }

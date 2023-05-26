acc6e45e88adc33ae9c15be65a2aa26efe3ce98b
RumeoLola <rumeomololaz@gmail.com>
2023-05-25 20:45:21 -0400

    unobstrusive event binding to fetch click food item

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-function openNav()  {
-  document.getElementById("mySidenav").style.width = "100%";
-}
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    function openNav()  {
+      document.getElementById("mySidenav").style.width = "100%";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+    
+    function addToCart()  {
+      var orderfood = document.querySelector("#foodname").textContent;
+      
+      var message = "The following food items have been added to your cart " + orderfood ;
+      
+      console.log("The food item " + orderfood + " has been added to your cart")
+      
+      document.getElementById("cartitems").textContent = message
+    
+    }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+    document.querySelector("button")
+      .addEventListener("click", addToCart);
 
-function addToCart()  {
-  var orderfood = document.querySelector("#foodname").textContent;
-  
-  var message = "The following food items have been added to your cart " + orderfood ;
-  
-  console.log("The food item " + orderfood + " has been added to your cart")
-  
-  document.getElementById("cartitems").innerHTML = message
-
-}
+  }
+);
